c0a665d34043931e7c280af339d4e45931481e08
cgudivad <chetana.gudivada@valpo.edu>
2023-05-03 00:43:07 +0000

    modified module source from ssh to https

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "network" {
 
-        source = "git@github.com:cgudivad/Terraform-Modules.git//network?ref=tags/V0.0.2"
+        source = "git::https://github.com/cgudivad/Terraform-Modules.git//network?ref=tags/V0.0.2"
 
         project                 = var.project
         vpcnetworkname          = var.vpcnetworkname
@@ -13,7 +13,7 @@ module "network" {
 
 module "vm" {
 
-        source = "git@github.com:cgudivad/Terraform-Modules//vm?ref=tags/V0.0.2"
+        source = "git::https://github.com/cgudivad/Terraform-Modules.git//vm?ref=tags/V0.0.2"
 
         project                 = var.project
         vpcnetworkname          = module.network.vpcnetworkname
@@ -26,7 +26,7 @@ module "vm" {
 
 module "private_vpc" {
 
-	source = "git@github.com:cgudivad/Terraform-Modules//private_vpc_connection?ref=tags/V0.0.2"
+	source = "git::https://github.com/cgudivad/Terraform-Modules.git//private_vpc_connection?ref=tags/V0.0.2"
 
 	project                 = var.project
 	privateipname		= var.privateipname
@@ -36,7 +36,7 @@ module "private_vpc" {
 
 module "sqldbinstance" {
 
-	source = "git@github.com:cgudivad/Terraform-Modules//sql_database_instance?ref=tags/V0.0.2"
+	source = "git::https://github.com/cgudivad/Terraform-Modules.git//sql_database_instance?ref=tags/V0.0.2"
 
 	project                 = var.project
 	region                  = var.region
